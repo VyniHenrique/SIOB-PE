2e96f6694a44465ad17c6362df85c90b41512834
Vynicius <vynicius.henrique15@gmail.com>
2026-01-15 23:37:01 -0300

    adicionando o UserDetailsService e o PasswordEncoder na API

--- backend/src/main/java/com/siob_pe/siob_pe/config/SecutityConfiguration.java
+++ backend/src/main/java/com/siob_pe/siob_pe/config/SecutityConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +25,25 @@ public class SecutityConfiguration {
                 .csrf( csrf -> csrf.disable())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(http -> http.anyRequest().permitAll())
+                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+
+        UserDetails user1 = User.builder()
+                .username("ADMIN")
+                .password(passwordEncoder.encode("ADMIN"))
+                .roles("USER")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user1);
     }
 }
