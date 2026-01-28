61d9267598c42c3921025af180d519177248ccf1
Vynicius <vynicius.henrique15@gmail.com>
2026-01-28 10:03:53 -0300

    bug fix

--- backend/src/main/java/com/siob_pe/siob_pe/model/Ocorrencia.java
+++ backend/src/main/java/com/siob_pe/siob_pe/model/Ocorrencia.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "ocorrencia", schema = "public")
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class Ocorrencia {
 
     @Id
--- backend/src/main/java/com/siob_pe/siob_pe/model/Usuario.java
+++ backend/src/main/java/com/siob_pe/siob_pe/model/Usuario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "usuario", schema = "public")
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario {
 
     @Id
